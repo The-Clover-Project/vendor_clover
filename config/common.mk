a94748f64409042bb959e9e692816d104036db0a
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-06-15 15:49:57 -0300

    vendor: Add permissions for lineagehw
    Change-Id: I55172ac4e9121aa969b628b450781e58151de18c

--- config/common.mk
+++ config/common.mk
@@ -193,6 +193,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/clover/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml
 
+# Lineage-specific file
+PRODUCT_COPY_FILES += \
+    vendor/clover/config/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lineagehw.xml
+
 # rsync
 PRODUCT_PACKAGES += \
     rsync
