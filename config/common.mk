e57e0bacee6474db99401dfba39eed77051fb7c2
Luofan Chen <clfbbn@gmail.com>
2025-02-01 02:03:58 +0530

    vendor: config: Copy Lineage health permissions Change-Id: I12dcb124293cac0ba6f7275ee30498b711c723dd

--- config/common.mk
+++ config/common.mk
@@ -184,6 +184,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/clover/config/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
 
+# Lineage Health
+PRODUCT_COPY_FILES += \
+    vendor/clover/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml
+
 # rsync
 PRODUCT_PACKAGES += \
     rsync
