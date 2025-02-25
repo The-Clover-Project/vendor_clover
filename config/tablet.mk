1ed8eedee84c027c48c40e7d464bb581dcfce563
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-25 22:20:10 +0800

    config: tablet: Include tablet-specific overlay
    Change-Id: I8235aad467d643b06d0ad1dd2c7c0de24bdf4679

--- config/tablet.mk
+++ config/tablet.mk
@@ -3,3 +3,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 # Settings
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.settings.large_screen_opt.enabled=true
+
+# Tablet-specific overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/clover/overlay/tablet
