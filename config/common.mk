6b64688ab7cbcf0764b91424bca44d7575163a18
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-04-06 09:52:46 +0545

    config: Update SystemUIGXOverlay package name
    Change-Id: Ie9c8486037381702a02dc2882f94eada6c7fd1cd

--- config/common.mk
+++ config/common.mk
@@ -181,7 +181,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlay
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural;com.google.android.systemui.gxoverlay
+    ro.boot.vendor.overlay.theme=com.google.android.systemui.gxoverlay_gms
 
 # Packages
 PRODUCT_PACKAGES += \
