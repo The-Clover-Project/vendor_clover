05482d403c95975043f67a160b1e57873d56ff19
RiteshSahany <SahanyRitesh@proton.me>
2026-01-09 23:09:25 +0530

    Revert "vendor: Initial import of themes"
    This reverts commit 3718cb20215b04900effaac8ca3f4886a870602b.

--- config/common.mk
+++ config/common.mk
@@ -7,9 +7,6 @@ $(call inherit-product, vendor/clover/config/bootanimation.mk)
 # Certification
 $(call inherit-product-if-exists, vendor/certification/config.mk)
 
-# Theme overlays
-$(call inherit-product-if-exists, vendor/clover/themes/config.mk)
-
 # Allow vendor prebuilt repos to exclude themselves from bp scanning
 -include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
 
@@ -320,3 +317,6 @@ $(call inherit-product, vendor/gms/products/gms.mk)
 include vendor/clover/config/version.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
+
+# Fonts
+include vendor/clover/config/fonts.mk
--- themes/config.mk
+++
@@ -1,8 +0,0 @@
-# Fonts
-include vendor/clover/themes/fonts/fonts.mk
-
-# Icon Packs
-include vendor/clover/themes/iconpacks/iconpacks.mk
-
-# Icon Shapes
-include vendor/clover/themes/iconshapes/iconshapes.mk
