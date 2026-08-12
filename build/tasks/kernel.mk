7a125d4856a8096af87b9dd66e9869c4a8f2e315
Yumi Yukimura <me.cafebabe@gmail.com>
2026-08-12 11:19:46 +0800

    fixup! kernel: Fix build error when modules finder returns nothing
    Change-Id: I3d568b5e822b8f9d7873b286502891427e4aff12

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -595,7 +595,7 @@ $(TARGET_PREBUILT_INT_KERNEL): $(KERNEL_CONFIG) $(DEPMOD) $(DTC) $(KERNEL_MODULE
 				[ $$? -ne 0 ] && exit 1; \
 				if [ -n "$$vendor_boot_modules" ]; then \
 					($(call build-image-kernel-modules-clover,$$vendor_boot_modules,$(KERNEL_VENDOR_RAMDISK_MODULES_OUT),,$(KERNEL_VENDOR_RAMDISK_DEPMOD_STAGING_DIR),$(KERNEL_VENDOR_RAMDISK_KERNEL_MODULES_LOAD),,,)) || exit "$$?"; \
-				fi \
+				fi; \
 			) \
 			$(if $(RECOVERY_KERNEL_MODULES)$(RECOVERY_KERNEL_MODULES_FINDER),\
 				$(if $(RECOVERY_KERNEL_MODULES_FINDER), \
@@ -611,7 +611,7 @@ $(TARGET_PREBUILT_INT_KERNEL): $(KERNEL_CONFIG) $(DEPMOD) $(DTC) $(KERNEL_MODULE
 				[ $$? -ne 0 ] && exit 1; \
 				if [ -n "$$recovery_modules" ]; then \
 					($(call build-image-kernel-modules-clover,$$recovery_modules,$(KERNEL_RECOVERY_MODULES_OUT),,$(KERNEL_RECOVERY_DEPMOD_STAGING_DIR),$(BOARD_RECOVERY_KERNEL_MODULES_LOAD),,,)) || exit "$$?"; \
-				fi \
+				fi; \
 			) \
 		fi
 
