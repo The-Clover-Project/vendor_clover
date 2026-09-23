84da612d4198077a0a90d5f0ce45c23e587610c0
RiteshSahany <ritesh@thecloverproject.com>
2026-09-23 10:05:11 +0000

    vendor: import clocks from mustang CP2A.260805.005
    Change-Id: I3e131f9091bc615a291de4b05c2db4859b8fe632

--- config/common.mk
+++ config/common.mk
@@ -122,6 +122,16 @@ endif
 PRODUCT_PACKAGES += \
     framework_compatibility_matrix.lineage.xml
 
+# Lockscreen Clocks
+PRODUCT_PACKAGES += \
+    SystemUIClocks-BigNum \
+    SystemUIClocks-Calligraphy \
+    SystemUIClocks-Growth \
+    SystemUIClocks-Inflate \
+    SystemUIClocks-Metro \
+    SystemUIClocks-NumOverlap \
+    SystemUIClocks-Weather
+
 ifeq ($(CLOVER_BUILDTYPE), OFFICIAL)
 # Clover packages
 PRODUCT_PACKAGES += \
