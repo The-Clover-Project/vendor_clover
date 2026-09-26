GenesisOS/vendor_genesis

vendor: config: Add google dialer call recording feature
Change-Id: Ifc8ab79aa5bfee81774af96943d668074fc36ee5
Signed-off-by: Frost <frost.github@proton.me>

config/common.mk
@@ -104,6 +104,14 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 PRODUCT_COPY_FILES += \
     vendor/genesis/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 
+# Call Recording
+TARGET_CALL_RECORDING_SUPPORTED ?= true
+
+ifneq ($(TARGET_CALL_RECORDING_SUPPORTED),false)
+PRODUCT_COPY_FILES += \
+    vendor/genesis/config/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
+endif
+
 # Charger
 PRODUCT_PACKAGES += \
     custom_charger_animation \

The-Clover-Project/vendor_clover

config/common.mk
@@ -69,6 +69,14 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/clover/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 
+# Call Recording
+TARGET_CALL_RECORDING_SUPPORTED ?= true
+
+ifneq ($(TARGET_CALL_RECORDING_SUPPORTED),false)
+PRODUCT_COPY_FILES += \
+    vendor/clover/config/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
+endif
+
 # Clover-specific init rc file
 PRODUCT_COPY_FILES += \
     vendor/clover/prebuilt/common/etc/init/init.clover-system_ext.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.clover-system_ext.rc